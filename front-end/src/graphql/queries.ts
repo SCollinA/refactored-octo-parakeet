60e6e430b0c77b5e78ff003d00e4cf1f59ee2d5a
Collin Argo <collinargo@gmail.com>
2020-01-11 15:20:23 -0500

    add more hoop queries

--- front-end/src/graphql/queries.ts
+++ front-end/src/graphql/queries.ts
@@ -1,10 +1,12 @@
 import gql from "graphql-tag";
 import {
 	COLLECTION_FULL_FRAGMENT,
+	HOOP_BASIC_FRAGMENT,
 	HOOP_FULL_FRAGMENT,
+	HOOP_WITH_IMAGE_FRAGMENT,
 } from "./fragments";
 
-export const GET_HOOPS = gql`
+export const GET_HOOPS_FULL = gql`
     query getHoops {
         Hoop {
             ...HoopFull
@@ -13,13 +15,49 @@ export const GET_HOOPS = gql`
     ${HOOP_FULL_FRAGMENT}
 `;
 
-export const GET_HOOP = gql`
+export const GET_HOOP_FULL = gql`
     query getHoop($id: ID!) {
         Hoop(id: $id) {
             ...HoopFull
         }
     }
     ${HOOP_FULL_FRAGMENT}
+`;
+
+export const GET_HOOPS_BASIC = gql`
+    query getHoops {
+        Hoop {
+            ...HoopBasic
+        }
+    }
+    ${HOOP_BASIC_FRAGMENT}
+`;
+
+export const GET_HOOP_BASIC = gql`
+    query getHoop($id: ID!) {
+        Hoop(id: $id) {
+            ...HoopBasic
+        }
+    }
+    ${HOOP_BASIC_FRAGMENT}
+`;
+
+export const GET_HOOPS_WITH_IMAGE = gql`
+    query getHoops {
+        Hoop {
+            ...HoopWithImage
+        }
+    }
+    ${HOOP_WITH_IMAGE_FRAGMENT}
+`;
+
+export const GET_HOOP_WITH_IMAGE = gql`
+    query getHoop($id: ID!) {
+        Hoop(id: $id) {
+            ...HoopWithImage
+        }
+    }
+    ${HOOP_WITH_IMAGE_FRAGMENT}
 `;
 
 export const GET_COLLECTIONS = gql`
