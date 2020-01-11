3ac3dca20610ec1d78787afc87a0fc38dbea61da
Collin Argo <collinargo@gmail.com>
2020-01-11 16:41:35 -0500

    reorganize generic components

--- front-end/src/components/generic/page-break/PageBreak.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import "./PageBreak.css";
-
-export default ({ vertical = false }: { vertical?: boolean }) =>
-	<div className="page-break-container">
-		<div className={`page-break${vertical ? " page-break--vertical" : ""}`}/>
-	</div>;
