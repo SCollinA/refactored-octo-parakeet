dcb9cd180de44ebc27f391f114907ed9167a9f81
Collin Argo <collinargo@gmail.com>
2020-03-28 13:55:40 -0400

    extract auto size image logic to generic components

--- front-end/src/components-collin/model/data/image/read-only/ColImage.tsx
+++ front-end/src/components-collin/model/data/image/read-only/ColImage.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { createRef, useState, useEffect } from "react";
+
+import ColLoading from "../../../../loading/ColLoading";
 
 import "../../ColData.css";
 
@@ -7,14 +9,50 @@ import "./ColImage.scss";
 export default ({
 	imageAlt,
 	imageSrc,
+	initialLoading = false,
+	loadingText = "* * * * * * *",
 }: {
 	imageAlt: string,
 	imageSrc: string,
+	initialLoading: boolean,
+	loadingText: string,
 }) => {
+	const imageRef = createRef<HTMLImageElement>();
+	const [windowAspectRatio, setWindowAspectRatio] = useState<number>(1);
+	const [imageWidthPercent, setImageWidthPercent] = useState(0);
+	const [loading, setLoading] = useState(initialLoading || !imageWidthPercent);
+	const imageOnLoad = (image: HTMLImageElement | null) => {
+		if (!!image) {
+			const imageAspectRatio = image.width / image.height;
+			const correctedAspectRatio = imageAspectRatio / windowAspectRatio;
+			const newImageWidthPercent = correctedAspectRatio * 100;
+			setImageWidthPercent(newImageWidthPercent);
+		}
+	};
+	useEffect(() => setLoading(!imageWidthPercent), [imageWidthPercent, windowAspectRatio])
+	useEffect(() => {
+		const image = imageRef.current;
+		const updateWindowDimensions = () => {
+			setWindowAspectRatio(window.innerWidth / window.innerHeight);
+			imageOnLoad(image);
+		};
+		window.addEventListener("resize", updateWindowDimensions);
+		return () => window.removeEventListener("resize", updateWindowDimensions);
+	});
 	return (
-		<img className="col-image"
-			src={imageSrc}
-			alt={imageAlt}
-		/>
+		<div className="col-image">
+			<ColLoading text={loadingText}
+				loading={loading}
+				fitChild={true}
+				preventClick={false}
+			>
+				<img ref={imageRef}
+					src={imageSrc}
+					alt={imageAlt}
+					onLoad={() => imageOnLoad(imageRef.current)}
+					style={!loading ? { width: `${imageWidthPercent}%`	}: {}}
+				/>
+			</ColLoading>
+		</div>
 	);
 };
--- front-end/src/components/images/image-db/ImageDB.tsx
+++ front-end/src/components/images/image-db/ImageDB.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from "@apollo/react-hooks";
 import { get } from "lodash/fp";
-import React, { createRef, useEffect, useState } from "react";
+import React from "react";
 
-import Loading from "../../../components-collin/loading/ColLoading";
+import ColImage from "../../../components-collin/model/data/image/read-only/ColImage";
+
+import "./ImageDB.scss";
 
 export default ({
 	id,
@@ -27,37 +29,14 @@ export default ({
 		variables: { id },
 	});
 	image = image || get(imageQueryImagePath, data) || "";
-	const imageRef = createRef<HTMLImageElement>();
-	const [windowAspectRatio, setWindowAspectRatio] = useState<number>(1);
-	useEffect(() => {
-		const updateWindowDimensions = () =>
-			setWindowAspectRatio(window.innerWidth / window.innerHeight);
-		window.addEventListener("resize", updateWindowDimensions);
-		return () => window.removeEventListener("resize", updateWindowDimensions);
-	});
-	const [imageWidthPercent, setImageWidthPercent] = useState(0);
-	const imageOnLoad = () => {
-		const imageDB = imageRef.current;
-		if (!!imageDB) {
-			const imageAspectRatio = imageDB.width / imageDB.height;
-			const correctedAspectRatio = imageAspectRatio / windowAspectRatio;
-			const newImageWidthPercent = correctedAspectRatio * 100;
-			setImageWidthPercent(newImageWidthPercent);
-		}
-	};
-	const loading = dataLoading || !!imageWidthPercent;
+	const loading = dataLoading;
 	return (
-		<Loading text={loadingText}
-			loading={loading}
-			fitChild={true}
-			preventClick={false}
-		>
-			<img ref={imageRef} className="ImageDB"
-				src={`data:image/jpeg;base64,${image}`}
-				alt={imageAltText}
-				onLoad={imageOnLoad}
-				style={{ width: `${imageWidthPercent}%`	}}
+		<div className="image-db">
+			<ColImage initialLoading={loading}
+				loadingText={loadingText}
+				imageSrc={`data:image/jpeg;base64,${image}`}
+				imageAlt={imageAltText}
 			/>
-		</Loading>
+		</div>
 	);
 };
