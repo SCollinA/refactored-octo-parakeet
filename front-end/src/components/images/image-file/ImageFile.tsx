840996c376f40ebbbe511f98910dd8ca46a0f3b6
Collin Argo <collinargo@gmail.com>
2020-03-28 11:33:21 -0400

    restyle layout

--- front-end/src/components/images/image-file/ImageFile.tsx
+++ front-end/src/components/images/image-file/ImageFile.tsx
@@ -1,5 +1,8 @@
 import Img from "gatsby-image";
-import React, { useEffect, useState } from "react";
+import React, {
+	// useEffect,
+	useState,
+} from "react";
 
 import Loading from "../../../components-collin/loading/ColLoading";
 
@@ -12,15 +15,15 @@ export default ({
 	file: IImageFile,
 	loadingText: string,
 }) => {
-	const [windowAspectRatio, setWindowAspectRatio] = useState<number>(1);
-	useEffect(() => {
-		const updateWindowDimensions = () =>
-			setWindowAspectRatio(window.innerWidth / window.innerHeight);
-		window.addEventListener("resize", updateWindowDimensions);
-		return () => window.removeEventListener("resize", updateWindowDimensions);
-	});
-	const correctedAspectRatio = file.childImageSharp.fluid.aspectRatio / windowAspectRatio;
-	const imageWidthPercent = correctedAspectRatio * 100;
+	// const [windowAspectRatio, setWindowAspectRatio] = useState<number>(1);
+	// useEffect(() => {
+		// const updateWindowDimensions = () =>
+			// setWindowAspectRatio(window.innerWidth / window.innerHeight);
+	// 	window.addEventListener("resize", updateWindowDimensions);
+	// 	return () => window.removeEventListener("resize", updateWindowDimensions);
+	// });
+	// const correctedAspectRatio = file.childImageSharp.fluid.aspectRatio / windowAspectRatio;
+	// const imageWidthPercent = correctedAspectRatio * 100;
 	const [loading, setLoading] = useState(false);
 	if (!!file) {
 		return (
@@ -33,7 +36,7 @@ export default ({
 					fluid={file.childImageSharp.fluid}
 					onStartLoad={() => setLoading(true)}
 					onLoad={() => setLoading(false)}
-					style={{ width: `${imageWidthPercent}%` }}
+					// style={{ width: `${imageWidthPercent}%` }}
 				/>
 			</Loading>
 		);
